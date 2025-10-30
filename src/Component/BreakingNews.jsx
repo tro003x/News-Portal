b91a712ee10bbdc407c65177ada1e7d6ee196a46
Auritro <oritrobinislam@gmail.com>
2025-10-31 00:54:23 +0600

    News details updated

--- src/Component/BreakingNews.jsx
+++ src/Component/BreakingNews.jsx
@@ -6,9 +6,9 @@ const BreakingNews = () => {
         <div className='flex items-center gap-5 bg-base-200 p-3'>
             <p className='text-base-100 bg-secondary px-3 py-2'>Latest</p>
             <Marquee className='flex gap-5' pauseOnHover ={true} speed={70}>
-                <p className='font-bold '>Hellow Beautiful people. It's me, Auritro. So this site was a fun project for me. </p>
+                <p className='font-bold '>Hellow Beautiful people. It's me, Auritro. So this site is a fun project for me. Made it for practice purpose. </p>
                 
-                <p className='font-bold'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit quibusdam sed tempore ducimus quisquam quas officiis, delectus modi quam a animi minima blanditiis perferendis, neque voluptatibus consequatur sequi amet! Architecto.</p>
+                <p className='font-bold'> This site has almost all the features that modern sites contains. It's mainly build with React.JS, tailwindCSS, DaisyUI, Firebase.</p>
             </Marquee>
         </div>
     );
